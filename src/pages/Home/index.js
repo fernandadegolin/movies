d3d63e8aabe1bb69d55572ff1f1876e4ecc2d230
fernandadegolin <fernandadegolin@hotmail.com>
2021-11-03 23:28:25 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './styles';
+
+
+export function Home() {
+    return(
+        <div>
+            <S.Title>ZeeDog</S.Title>
+        </div>
+
+
+    )
+}
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  
+`;
+
+export const Title = styled.h1`
+    text-align: center;
+`;
